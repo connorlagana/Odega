ce3ccfe1af6dd5e3941a61b89d3bd5f3c2e66391
Connor Lagana <connor.lagana@gmail.com>
2020-02-24 13:05:20 -0500

    simplified nav

--- src/components/main/MainNav.js
+++ src/components/main/MainNav.js
@@ -9,7 +9,13 @@ function MainNav() {
         <div id="odegaTitle">Odega</div>
         <div id="studiosTitle">Studios</div>
       </div>
-      <div className="mainNavRightDiv"></div>
+      <div className="mainNavRightDiv">
+        <div className="mainNavIndy">Home</div>
+        <div className="mainNavIndy">What We Do</div>
+        <div className="mainNavIndy">Our Work</div>
+        <div className="mainNavIndy">About Us</div>
+        <div className="mainNavIndy">Contact Us</div>
+      </div>
     </div>
   );
 }
