1e60fdfaa404f59cc3086dfd2f40f60fbe0381a5
Connor Lagana <connor.lagana@gmail.com>
2020-02-24 12:59:29 -0500

    main nav left div

--- src/components/main/MainHero.js
+++ src/components/main/MainHero.js
@@ -2,7 +2,11 @@ import React from "react";
 import MainNav from "./MainNav.js";
 
 function MainHero() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MainNav />
+    </div>
+  );
 }
 
 export default MainHero;
--- src/components/main/MainNav.js
+++ src/components/main/MainNav.js
@@ -3,8 +3,13 @@ import odegaLogo from "../../images/OdegaLogo.png";
 
 function MainNav() {
   return (
-    <div className="App">
-      <img src={odegaLogo} />
+    <div className="nav">
+      <div className="mainNavLeftDiv">
+        <img id="odegaLogo" src={odegaLogo} />
+        <div id="odegaTitle">Odega</div>
+        <div id="studiosTitle">Studios</div>
+      </div>
+      <div className="mainNavRightDiv"></div>
     </div>
   );
 }
