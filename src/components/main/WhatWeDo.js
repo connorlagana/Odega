8d2d22d589a106975af2da262537420c5a2f322d
Connor Lagana <connor.lagana@gmail.com>
2020-02-25 17:29:25 -0500

    what we do page

--- src/components/main/WhatWeDo.js
+++ src/components/main/WhatWeDo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import mobileDev from "../../images/mobileDev.png";
 import webDev from "../../images/webDev.png";
+import Fade from "react-reveal/Fade";
 
 function WhatWeDo() {
   return (
@@ -9,23 +10,27 @@ function WhatWeDo() {
         <div>
           <img src={mobileDev} />
         </div>
-        <div id="devTitle">Mobile App Development</div>
-        <div id="devDesc">
-          Our team of programmers and designers are working together to deliver
-          you iOS and Android apps that can be used by everyone for every day
-          needs.
-        </div>
+        <Fade left>
+          <div id="devTitle">Mobile App Development</div>
+          <div id="devDesc">
+            Our team of programmers and designers are working together to
+            deliver you iOS and Android apps that can be used by everyone for
+            every day needs.
+          </div>
+        </Fade>
       </div>
       <div className="wwdIndy">
         <div>
           <img src={webDev} />
         </div>
-        <div id="devTitle">Web Development</div>
-        <div id="devDesc">
-          Along with mobile apps, Odega Studios plans on creating web based
-          versions of your favorite mobile apps, so you can enjoy Sound Flex,
-          TradrX, or any of our other apps anywhere you go.
-        </div>
+        <Fade right>
+          <div id="devTitle">Web Development</div>
+          <div id="devDesc">
+            Along with mobile apps, Odega Studios plans on creating web based
+            versions of your favorite mobile apps, so you can enjoy Sound Flex,
+            TradrX, or any of our other apps anywhere you go.
+          </div>
+        </Fade>
       </div>
     </div>
   );
